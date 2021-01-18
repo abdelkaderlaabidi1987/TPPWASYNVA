1e7a9d7f9e59e76954676a13e256f5a8b174884c
abdelkaderlaabidi1987 <60947747+abdelkaderlaabidi1987@users.noreply.github.com>
2021-01-18 13:15:32 +0100

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener(type: 'install', listener: evt=>
-                      {console.log(evt)
-                      });
+                     {console.log(evt);
+                      })
 self.addEventListener(type: 'activate', listener: evt=>
-                      {console.log(evt)
-                      });
+                      {console.log(evt);
+                      })
