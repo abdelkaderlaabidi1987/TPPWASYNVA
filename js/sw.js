602e373874968ae27b229b4ab309f0b1443c49fc
abdelkaderlaabidi1987 <60947747+abdelkaderlaabidi1987@users.noreply.github.com>
2021-01-18 12:04:19 +0100

    Update sw.js
    ajouter wahd service wlaker

--- js/sw.js
+++ js/sw.js
@@ -1,1 +1,6 @@
-isicizdcksd
+self.addEventListener(type: 'install', listener: evt=>
+                      {console.log(evt)
+                      });
+self.addEventListener(type: 'activate', listener: evt=>
+                      {console.log(evt)
+                      });
