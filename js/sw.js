4b9be31a89c6bcb8f4bfddf52e9a819d54d73aef
abdelkaderlaabidi1987 <60947747+abdelkaderlaabidi1987@users.noreply.github.com>
2021-01-19 23:56:35 +0100

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -5,3 +5,10 @@ self.addEventListener('install', evt=>{
 self.addEventListener('activate', evt=>{
     console.log(evt);
 })
+
+self.addEventListener('fetch', evt=>{
+    if (!navigator.onLine){
+        evt.respondWith( new Response('pas de connexion internet'))
+    }
+    console.log(evt.request.url);
+})
