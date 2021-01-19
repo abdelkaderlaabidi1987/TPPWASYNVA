fcf20c023995f2e68f3a9ae495b3510dc7b8505f
abdelkaderlaabidi1987 <60947747+abdelkaderlaabidi1987@users.noreply.github.com>
2021-01-20 00:50:58 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', evt=>{
+    console.log(evt);
+})
+self.addEventListener('activate', evt=>{
+    console.log(evt);
+})
+
+self.addEventListener('fetch', evt=>{
+    if (!navigator.onLine){
+        evt.respondWith( new Response('pas de connexion internet'))
+    }
+    console.log(evt.request.url);
+})
